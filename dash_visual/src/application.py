4c742fe465b763fd087d41904729b375e9a06e97
tubone <tubo.yyyuuu@gmail.com>
2019-03-31 10:34:14 +0900

    Create dash

--- dash_visual/src/application.py
+++ dash_visual/src/application.py
@@ -5,11 +5,14 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-df = pd.read_csv("result.csv")
+df_all = pd.read_csv("result_all.csv")
+df_kanto = pd.read_csv("result_kanto.csv")
+df_kanto_groupby = df_kanto.groupby("station_id", as_index=False).mean()
+
 # Or using s3 bucket.
 # df = pd.read_csv("s3://your-bucket/result.csv")
 
-mapbox_access_token = "your token"
+mapbox_access_token = "pk.eyJ1IjoidHVib25lIiwiYSI6ImNqdG85aDhuZTBramE0NG4ybGFxNWRsM3IifQ.2RyoyqPBvjrBHRb8Hk1ciQ"
 # FIXME: input your mapbox token
 # https://docs.mapbox.com/help/how-mapbox-works/access-tokens/
 
@@ -17,28 +20,28 @@ app = dash.Dash()
 application = app.server
 
 app.layout = html.Div(children=[
-    html.H1(children="温度マップ"),
+    html.H1(children="全国の温度マップ"),
 
     dcc.Graph(
         id="temp-map",
         figure={
             "data": [
                 go.Scattermapbox(
-                    lat=df[df["station_id"] == i]["latitude"],
-                    lon=df[df["station_id"] == i]["longitude"],
+                    lat=df_all[df_all["prefecture"] == i]["latitude"],
+                    lon=df_all[df_all["prefecture"] == i]["longitude"],
                     mode="markers",
                     marker=dict(
                         symbol="circle",
                         size=16,
                         opacity=0.8,
                         colorscale="RdBu",
-                        cmin=df["avg_temperature"].min(),
-                        color=df[df["station_id"] == i]["avg_temperature"],
-                        cmax=df["avg_temperature"].max(),
+                        cmin=df_all["avg_temperature"].min(),
+                        color=df_all[df_all["prefecture"] == i]["avg_temperature"],
+                        cmax=df_all["avg_temperature"].max(),
                     ),
-                    text=df[df["station_id"] == i]["avg_temperature"],
-                    name=str(df[df["station_id"] == i]["first_name"].values),
-                ) for i in df["station_id"].unique()
+                    text=df_all[df_all["prefecture"] == i]["avg_temperature"],
+                    name=str(df_all[df_all["prefecture"] == i]["prefecture"].values),
+                ) for i in df_all["prefecture"].unique()
             ],
             'layout':
                 go.Layout(
@@ -48,8 +51,8 @@ app.layout = html.Div(children=[
                         accesstoken=mapbox_access_token,
                         bearing=0,
                         center=dict(
-                            lat=np.mean(df["latitude"]),
-                            lon=np.mean(df["longitude"])
+                            lat=np.mean(df_all["latitude"]),
+                            lon=np.mean(df_all["longitude"])
                         ),
                         pitch=100,
                         zoom=6,
@@ -57,9 +60,49 @@ app.layout = html.Div(children=[
                     height=900
                 )
         }
+    ),
+    dcc.Graph(
+        id="tempkanto-map",
+        figure={
+            "data": [
+                go.Scattermapbox(
+                    lat=df_kanto_groupby[df_kanto_groupby["station_id"] == i]["latitude"],
+                    lon=df_kanto_groupby[df_kanto_groupby["station_id"] == i]["longitude"],
+                    mode="markers",
+                    marker=dict(
+                        symbol="circle",
+                        size=16,
+                        opacity=0.8,
+                        colorscale="RdBu",
+                        cmin=df_kanto_groupby["avg_temperature"].min(),
+                        color=df_kanto_groupby[df_kanto_groupby["station_id"] == i]["avg_temperature"],
+                        cmax=df_kanto_groupby["avg_temperature"].max(),
+                    ),
+                    text=df_kanto_groupby[df_kanto_groupby["station_id"] == i]["avg_temperature"],
+                    name=str(df_kanto_groupby[df_kanto_groupby["station_id"] == i]["station_id"].values),
+                ) for i in df_kanto_groupby["station_id"].unique()
+            ],
+            'layout':
+                go.Layout(
+                    autosize=True,
+                    hovermode="closest",
+                    mapbox=dict(
+                        accesstoken=mapbox_access_token,
+                        bearing=0,
+                        center=dict(
+                            lat=np.mean(df_kanto_groupby["latitude"]),
+                            lon=np.mean(df_kanto_groupby["longitude"])
+                        ),
+                        pitch=100,
+                        zoom=8,
+                    ),
+                    height=900
+                )
+        }
     )
-
 ])
+
+
 
 if __name__ == "__main__":
     application.run(debug=True, port=8080)
--- dash_visual/src/download_csv.py
+++ dash_visual/src/download_csv.py
@@ -8,9 +8,12 @@ RESULT_BUCKET = sys.argv[1]
 
 
 def main():
-    query_exeqution_id = select_weather(athena)
-    print("Athena Run success")
-    download_csv_to_local(s3, RESULT_BUCKET, query_exeqution_id)
+    query_exeqution_id = select_avgtemp_by_prefecture(athena)
+    print("Athena select_avgtemp_by_prefecture Run success")
+    download_csv_to_local(s3, RESULT_BUCKET, query_exeqution_id, "result_all.csv")
+    query_exeqution_id = select_avgtemp_at_kanto(athena)
+    print("Athena select_avgtemp_at_kanto Run success")
+    download_csv_to_local(s3, RESULT_BUCKET, query_exeqution_id, "result_kanto.csv")
 
 
 def select_weather(athena):
@@ -38,7 +41,7 @@ def select_weather(athena):
       FROM "weather_v2"."weather_with_station_id"
       WHERE avg_temperature != '' AND
             high_temperature != '' AND
-            low_temperature != '' AND) as weather
+            low_temperature != '') as weather
     LEFT JOIN ( 
       SELECT id, 
              prefecture,
@@ -50,6 +53,73 @@ def select_weather(athena):
       FROM "weather_v2"."station") as station
     ON weather.station_id = station.id
     GROUP BY station_id;
+    """
+    results = execute_athena_query(athena, sql)
+    return results
+
+
+def select_avgtemp_by_prefecture(athena):
+    sql = """
+        SELECT 
+       MAX(station.prefecture) AS prefecture,
+       AVG(CAST(weather.avg_temperature AS double)) AS avg_temperature,
+       MAX(station.latitude) AS latitude,
+       MAX(station.longitude) AS longitude,
+       MAX(station.altitude) AS altitude
+    FROM (
+      SELECT datetime, 
+        avg_temperature, 
+        precipitation, 
+        hours_sunlight, 
+        avg_wind_speed,
+        station_id
+      FROM "weather_v2"."weather_with_station_id"
+      WHERE avg_temperature != '') as weather
+    LEFT JOIN ( 
+      SELECT id, 
+             prefecture,
+             latitude,
+             longitude,
+             altitude
+      FROM "weather_v2"."station") as station
+    ON weather.station_id = station.id
+    GROUP BY station.prefecture;
+    """
+    results = execute_athena_query(athena, sql)
+    return results
+
+
+def select_avgtemp_at_kanto(athena):
+    sql = """
+    SELECT 
+       weather.station_id,
+       weather.datetime,
+       weather.avg_temperature,
+       station.prefecture,
+       station.latitude,
+       station.longitude,
+       station.altitude
+    FROM (
+      SELECT datetime, 
+        avg_temperature, 
+        station_id
+      FROM "weather_v2"."weather_with_station_id"
+      WHERE avg_temperature != '') as weather
+    LEFT JOIN ( 
+      SELECT id, 
+             prefecture,
+             latitude,
+             longitude,
+             altitude
+      FROM "weather_v2"."station") as station
+    ON weather.station_id = station.id
+    WHERE station.prefecture = 'tokyo' OR
+              station.prefecture = 'kanagawa' OR
+              station.prefecture = 'saitama' OR
+              station.prefecture = 'chiba' OR
+              station.prefecture = 'gunma' OR
+              station.prefecture = 'tochigi' OR
+              station.prefecture = 'ibaraki';
     """
     results = execute_athena_query(athena, sql)
     return results
@@ -75,8 +145,8 @@ def execute_athena_query(athena, sql):
     return query_exeqution_id
 
 
-def download_csv_to_local(s3, result_bucket, query_exeqution_id):
-    s3.meta.client.download_file(result_bucket, query_exeqution_id + ".csv", "./result.csv")
+def download_csv_to_local(s3, result_bucket, query_exeqution_id, local_name):
+    s3.meta.client.download_file(result_bucket, query_exeqution_id + ".csv", local_name)
 
 
 if __name__ == "__main__":
